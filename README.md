aceefcd712f20016b378af2cdd6a876d121d29f9
Alexandre Castro <alex.monteiro@seznam.cz>
2018-07-29 20:56:24 +0200

    setting up prject repo, initial files, initial components and react-router-dom + Navigation is ready

---
+++ README.md
@@ -0,0 +1,11 @@
+# Portfolio Site with ReactJs
+
+Website was built using ReactJs
+
+## Features:
+
+* React Router
+* Stateful and Stateless Components
+* Responsive Design
+* Overlay
+
---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import './App.css';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import Main from './components/main';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="demo-big-content">
+    <Layout>
+        <Header title="My Portfolio" scroll>
+            <Navigation>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/about">About Me</NavLink>
+                <NavLink to="/resume">Resumé</NavLink>
+                <NavLink to="/projects">Projects</NavLink>
+                <NavLink to="/contact">Contact Me</NavLink>
+            </Navigation>
+        </Header>
+        <Drawer title="My Portfolio">
+            <Navigation>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/about">About Me</NavLink>
+                <NavLink to="/resume">Resumé</NavLink>
+                <NavLink to="/projects">Projects</NavLink>
+                <NavLink to="/contact">Contact Me</NavLink>
+            </Navigation>
+        </Drawer>
+        <Content>
+            <div className="page-content" />
+            <Main />
+        </Content>
+    </Layout>
+</div>
+      </div>
+    );
+  }
+}
+
+export default App;
